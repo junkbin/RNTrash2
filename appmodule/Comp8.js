9a3969b60a520f3e43e92d555ee0ce76f952e138
santosh <santosh.ece06@gmail.com>
2018-11-23 14:55:30 +0530

    comp 8

---
+++ appmodule/Comp8.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {styles} from './Comp8Style';
+export class Comp8 extends React.Component{
+
+    bglist = ['gold', 'green',  'royalblue', 'tomato',  '#f44336', '#e91e63', '#9c27b0', '#2196f3', '#8bc34a', '#607d8b'];
+
+    constructor(props){
+        super(props);
+        this.state = {'bglist': this.bglist, 'bgfy' : styles.bgfy};
+    }
+
+    componentDidMount(){
+        setInterval(()=>{this.changeBackground()}, 2000);
+    }
+
+    changeBackground() {
+        try{
+            const refstyles = JSON.parse(JSON.stringify(styles));
+            const random = Math.floor(Math.random() * this.bglist.length);
+            refstyles.bgfy.backgroundColor = this.bglist[random];
+            
+            const newRefState = {'bgfy': refstyles.bgfy};
+            this.setState(newRefState);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    render(){
+        return (
+            <View style={[styles.contaienr, this.state.bgfy]}>
+                <Text style={styles.textify}>Helloo</Text>
+            </View>
+        );
+    }
+}
---
+++ appmodule/Comp8Style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    contaienr : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    bgfy : {
+        backgroundColor: 'tomato'
+    },
+
+    textify : {
+        color : 'white',
+        fontSize : 40,
+        fontWeight : 'bold',
+        fontFamily : 'cursive'
+    }
+});
+
+
+export {styles}
