374e42cf5f9e8f17914981168e37a04e1d6c3f82
santosh <santosh.ece06@gmail.com>
2018-11-23 16:05:58 +0530

    Coponent 8

--- appmodule/Comp8.js
+++ appmodule/Comp8.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import * as BPromise from 'bluebird';
 
 import {styles} from './Comp8Style';
 export class Comp8 extends React.Component{
@@ -8,11 +9,11 @@ export class Comp8 extends React.Component{
 
     constructor(props){
         super(props);
-        this.state = {'bglist': this.bglist, 'bgfy' : styles.bgfy};
+        this.state = {'bglist': this.bglist, 'bgfy' : styles.bgfy, 'title': 'Hello World', 'display': ''};
     }
 
     componentDidMount(){
-        setInterval(()=>{this.changeBackground()}, 2000);
+        setTimeout(()=>{this.changeBackground()}, 2000);
     }
 
     changeBackground() {
@@ -23,15 +24,50 @@ export class Comp8 extends React.Component{
             
             const newRefState = {'bgfy': refstyles.bgfy};
             this.setState(newRefState);
+
+            let taskList = [];
+            for(let i=0; i<this.state.title.length; i++){
+                let refdata = this.state.title.substring(0, i+1);
+                taskList.push(refdata);
+            }
+
+            BPromise.reduce(taskList, (total, task)=>{
+                return this.changeContent(task).then((data)=>{
+                    return total + 1;
+                })
+            }).then(()=>{
+
+                setTimeout(()=>{
+                    this.changeBackground();
+                }, 500);
+                
+            });
         }catch(err){
             console.log(err);
         }
     }
 
+     changeContent(data) {
+        let sthis = this; 
+        return new Promise((resolve, reject)=>{
+            try{
+                setTimeout(()=>{
+                    console.log(data);
+                    sthis.setState({display : data});
+
+                    resolve(data);
+                }, 250);
+            }catch(err){
+                console.log(err);
+                reject(err);
+            }
+        });
+    }
+
     render(){
         return (
             <View style={[styles.contaienr, this.state.bgfy]}>
-                <Text style={styles.textify}>Helloo {this.state.bgfy.backgroundColor}</Text>
+                <Text style={styles.textify}>{this.state.display}</Text>
             </View>
         );
     }
