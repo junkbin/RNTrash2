40eb0ae738248715f12800e3855cc97106af195d
santosh <santosh.ece06@gmail.com>
2018-11-23 14:32:52 +0530

    Component 7

--- appmodule/Comp7.js
+++ appmodule/Comp7.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-
+import {styles} from './Comp7Style';
 export class Comp7 extends React.Component{
 
     render(){
         return (
-            <View>
-                <Text>Helloo</Text>
+            <View style={[styles.contaienr, styles.bgfy]}>
+                <Text style={styles.textify}>Helloo</Text>
             </View>
         );
     }
---
+++ appmodule/Comp7Style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    contaienr : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    bgfy : {
+        backgroundColor: 'royalblue'
+    },
+
+    textify : {
+        color : 'white',
+        fontSize : 30,
+        fontWeight : 'bold',
+        fontFamily : 'monospace'
+    }
+});
+
+
+export {styles}
