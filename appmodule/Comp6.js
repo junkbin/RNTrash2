e9f075dcd1b5febfd74caf33877d66dce20e9c2e
santosh <santosh.ece06@gmail.com>
2018-11-23 12:57:37 +0530

    comp 6

--- appmodule/Comp6.js
+++ appmodule/Comp6.js
@@ -8,13 +8,24 @@ export class Comp6 extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {'title' : 'Helloo Universe!!'};
+        this.state = {'title' : 'Helloo Universe!!', 'style6' : styles6};
     }
 
+    componentDidMount() {
+        setInterval(this.procesChangeBackground, 2000);
+    }
+
+    procesChangeBackground() {
+        try{
+            this.setState({"style6": {"bgfy": 'tomato'}, "title": "World"});
+        }catch(err){
+            console.log(err);
+        }
+    }
 
     render(){
         return (
-            <View style={[styles6.contianer, styles6.bgfy]}>
+            <View style={[styles6.contianer, this.state.style6.bgfy]}>
                 <Text style={styles6.textify}>
                     {this.state.title}
                 </Text>
