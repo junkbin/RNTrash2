329c3de9cf9cbf83096f9a2960e44ebf4111850d
santosh <santosh.ece06@gmail.com>
2018-11-23 11:06:57 +0530

    Compoenent 1

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+import {Comp1 as AppCompoent} from './app';
+
+AppRegistry.registerComponent(appName, () => AppCompoent);
