9e5b6f919efb271dd9dd7d83ff5028ba09df3f6c
santosh <santosh.ece06@gmail.com>
2018-11-23 12:47:10 +0530

    comp 6

--- appmodule/Comp6.js
+++ appmodule/Comp6.js
@@ -4,11 +4,19 @@ import {View, Text} from 'react-native';
 import {styles6} from './Comp6Style';
 
 export class Comp6 extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {'title' : 'Helloo Universe!!'};
+    }
+
+
     render(){
         return (
             <View style={[styles6.contianer, styles6.bgfy]}>
                 <Text style={styles6.textify}>
-                    Hello World
+                    {this.state.title}
                 </Text>
             </View>
         );
